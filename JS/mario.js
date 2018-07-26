707404a68991b3bcd6dafd01809bbb7b81bcac9d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-25 23:44:07 -0500

    Created function that returns given number of spaces

--- JS/mario.js
+++ JS/mario.js
@@ -1,1 +1,14 @@
 console.log('Mario');
+
+const spaces = (num) => {
+	let space = "";
+
+	for (let i = 0; i < num; i++) {
+
+		space += " ";
+	}
+
+	return space;
+};
+
+console.log(`Hello,${spaces(5)}World!`);
